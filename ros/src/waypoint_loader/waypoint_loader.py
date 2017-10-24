35d45d906adbc065beb73fad28d1198d2cf2681f
Alexey <alexey.simonov@gmail.com>
2017-10-24 12:05:37 +0100

    adding documentation for waypoint_loader.py

--- ros/src/waypoint_loader/waypoint_loader.py
+++ ros/src/waypoint_loader/waypoint_loader.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
+"""
+Waypoint loader.
 
+Loads waypoints from a file.
+Reads the velosity, in KPH from file.
+Updates waypoints with the target velosity (in M/S).
+Sets last waypoints velosity so the car decelerates and stops at the end.
+Publishes the calculated waypoints once to /base_waypoints topic.
+"""
 import os
 import csv
 import math
