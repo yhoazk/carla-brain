5293f6f5cc432a902c70309427891d9305fb9850
Peter Luptak <peterluptak@MacBook-Air.local>
2017-10-27 20:28:37 -0700

    First rough version of classification based on CV2

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,4 +1,11 @@
 from styx_msgs.msg import TrafficLight
+import rospy
+import numpy as np
+#import matplotlib.pyplot as plt
+import os
+import cv2
+
+
 
 class TLClassifier(object):
     def __init__(self):
@@ -16,4 +23,57 @@ class TLClassifier(object):
 
         """
         #TODO implement light color prediction
-        return TrafficLight.UNKNOWN
+
+        rospy.logwarn("tl_classifier: Classification requested")
+
+        l_channel = cv2.cvtColor(image, cv2.COLOR_RGB2LUV)[:,:,0]
+
+        img_h, img_w = l.shape
+
+        top_third_marker = int(img_h / 3)
+        bottom_third_marker = img_h - top_third_marker
+
+        top = 0
+        mid = 0
+        bottom = 0
+
+        count_result = {'RED': 0 , 'YELLOW': 0, 'GREEN': 0}
+
+        for i in range(top_third_marker):
+            for j in range(img_w):
+                top += l[i][j]
+        count_result['RED'] = top
+        
+
+        for i in range(top_third_marker, bottom_third_marker):
+            for j in range(img_w):
+                mid += l[i][j]
+        count_result['YELLOW'] = mid
+
+
+        for i in range(bottom_third_marker, img_h):
+            for j in range(img_w):
+                bottom += l[i][j]
+        count_result['GREEN'] = bottom
+
+
+        #evaluate which color is most likely
+
+        max_count = max(d, key=d.get)
+
+        if max_count == 'RED':
+            return TrafficLight.RED
+            rospy.logwarn("tl_classifier: RED light detected") 
+
+        elif max_count == 'YELLOW':
+            return TrafficLight.YELLOW
+            rospy.logwarn("tl_classifier: YELLOW light detected") 
+
+        elif max_count == 'GREEN':
+            return TrafficLight.GREEN
+            rospy.logwarn("tl_classifier: RED light detected") 
+
+        else:
+            return TrafficLight.UNKNOWN
+            rospy.logwarn("tl_classifier: ERROR - cannot classify light")
+
