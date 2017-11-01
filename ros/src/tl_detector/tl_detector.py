3c17446157ffea2bc42637311bf2a6a3f4ba243c
Peter Luptak <peterluptak@MacBook-Air.local>
2017-11-02 00:21:25 +0100

    minor cleanup, mostly of unused / duplicate code in classifier and detector.

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -124,12 +124,10 @@ class TLDetector(object):
             x2 = box[1][0]
             y2 = box[1][1]
             tl_image = cv_image[y1:y2, x1:x2]
-            #classifier_size = (128,128)
-            #resized = cv2.resize(tl_image, classifier_size, cv2.INTER_LINEAR)
-            resized = tl_image
+   
             # Classification
-            rospy.logwarn("tl_detector:About to call classifier")
-            tl_class = self.classifier.get_classification(resized)
+            rospy.logdebug("tl_detector:About to call classifier")
+            tl_class = self.classifier.get_classification(tl_image)
             classification[tl_class] += 1
             # debug output
             color = [255,255,255]
